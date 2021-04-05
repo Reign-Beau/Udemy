5d871ca91868b69ff6cd20edf10586bb1568488f
Justin Hall <justin.t.hall@outlook.com>
2021-04-04 19:13:21 -0500

    First commit, PHP

---
+++ hello_world.php
@@ -0,0 +1,9 @@
+<?php
+    echo "Hello World";
+    echo "Short hand echo looks like '<?=text in quotations;?>'";
+// comment out line
+/*multi
+line comment*/
+# comment line again
+
+
---
+++ variables.php
@@ -0,0 +1,8 @@
+<?php
+$variableRule1 = "Everything starts with a $";
+$variableRule2 = "Variables cannot start with a number";
+$variableRule3 = "All variables must start with a letter or underscore";
+$variableRule4 = "No spaces are allowed";
+$variableRule5 = "Numbers don't need quotes, everything else does";
+$variableRule6 = "PHP variables are case sensitive";
+echo $variableRule1, $variableRule2, $variableRule3, $variableRule4, $variableRule5, $variableRule6;
